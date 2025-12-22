7d7a223a5f4f807c883d0ceda64b64d9a5370407
sami157 <sami.eee157@gmail.com>
2025-12-22 18:00:22 +0600

    Leaderboard polishing

--- src/pages/LeaderBoard.jsx
+++ src/pages/LeaderBoard.jsx
@@ -1,49 +1,49 @@
 import useLeaderboard from "../hooks/useLeaderboard";
+import Loading from '../components/Loading'
 
 const Leaderboard = () => {
     const { data: leaderboard = [], isLoading } = useLeaderboard();
 
-    if (isLoading) {
-        return <div className="py-16 text-center">Loading leaderboard...</div>;
-    }
 
     return (
-        <section className="py-20 bg-gray-50">
-            <div className="max-w-5xl mx-auto px-4">
-                <h1 className="text-4xl font-bold text-center mb-10">
-                    Leaderboard
-                </h1>
+        <section className="py-20 bg-gray-50 min-h-screen">
+            <h1 className="text-5xl font-bold text-center mb-10">
+                Leaderboard
+            </h1>
+            {
+                isLoading ? <Loading /> :
+                    <div className="max-w-5xl mx-auto px-4">
+                        <div className="bg-white rounded-xl overflow-hidden">
+                            <table className="table w-full">
+                                <thead>
+                                    <tr>
+                                        <th className="py-3 px-4">Rank</th>
+                                        <th className="py-3 px-4">Name</th>
+                                        <th className="text-center py-3 px-4">Wins</th>
+                                    </tr>
+                                </thead>
 
-                <div className="bg-white rounded-xl overflow-hidden">
-                    <table className="table w-full">
-                        <thead>
-                            <tr>
-                                <th className="py-3 px-4">Rank</th>
-                                <th className="py-3 px-4">Name</th>
-                                <th className="text-center py-3 px-4">Wins</th>
-                            </tr>
-                        </thead>
+                                <tbody>
+                                    {leaderboard.map((user, index) => (
+                                        <tr key={user.userEmail} className="border-t">
+                                            <td className="py-3 px-4 font-semibold">
+                                                #{index + 1}
+                                            </td>
 
-                        <tbody>
-                            {leaderboard.map((user, index) => (
-                                <tr key={user.userEmail} className="border-t">
-                                    <td className="py-3 px-4 font-semibold">
-                                        #{index + 1}
-                                    </td>
+                                            <td className="py-3 px-4 flex items-center gap-3">
+                                                <span>{user.userName}</span>
+                                            </td>
 
-                                    <td className="py-3 px-4 flex items-center gap-3">
-                                        <span>{user.userName}</span>
-                                    </td>
-
-                                    <td className="py-3 px-4 text-center font-bold">
-                                        {user.wins}
-                                    </td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
-            </div>
+                                            <td className="py-3 px-4 text-center font-bold">
+                                                {user.wins}
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+            }
         </section>
     );
 };
